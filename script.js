9f3a83825e2c64b12d549820d436e4ebd3cb5f49
Julio Nepomuceno <julioreplay@gmail.com>
2020-12-13 19:00:21 -0300

    iniciando o page cases

---
+++ script.js
@@ -0,0 +1,5 @@
+function checkedMenu() {
+    document.getElementById('menu').classList.toggle('checked')
+    document.getElementById('nav').classList.toggle('checked')
+    document.getElementById('menu-bg').classList.toggle('checked-bg')
+}
