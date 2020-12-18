5f334cdbe1341b89d30120a67b5f557c74a18148
Julio Nepomuceno <julioreplay@gmail.com>
2020-12-18 15:25:09 -0300

    iniciando menu hamburguer

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -1,5 +1,22 @@
-function checkedMenu() {
-    document.getElementById('menu').classList.toggle('checked')
-    document.getElementById('nav').classList.toggle('checked')
-    document.getElementById('menu-bg').classList.toggle('checked-bg')
+const checkbox = document.getElementById('menu-hamburguer')
+const appearMenu = document.getElementById('appear-menu')
+
+function menuHamburguer() {
+    if (checkbox.checked) {
+        document.getElementById('menu').classList.toggle('marked')
+        document.getElementById('nav').classList.toggle('marked')
+
+        document.getElementById('section').style.display = "none"
+    
+        appearMenu.style.display="flex"
+    }
+
+    if (!checkbox.checked) {
+        document.getElementById('menu').classList.toggle('marked')
+        document.getElementById('nav').classList.toggle('marked')
+
+        document.getElementById('section').style.display = "block"
+    
+        appearMenu.style.display="none"
+    }
 }
