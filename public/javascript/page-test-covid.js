4a1fe348c2c6a38c5494a6c0b66389dd9393fcf1
Julio Nepomuceno <julioreplay@gmail.com>
2020-12-23 18:35:54 -0300

    adicionando popups do form

--- public/javascript/page-test-covid.js
+++ public/javascript/page-test-covid.js
@@ -1,5 +1,43 @@
+setTimeout( () => {
+
+    document.querySelector('.pop-ups').style.display ="flex"
+    document.querySelector('.test-explanation').style.display ="flex"
+},2000)
+
 function alerta() {
-    alert('ola')
+    document.querySelector('.pop-ups').style.display ="flex"
+    document.querySelector('.information-data').style.display ="flex"
+    
 }
 
 
+function hidePopup() {
+    document.querySelector('.pop-ups').style.display ="none"
+    document.querySelector('.test-explanation').style.display ="none"
+    document.querySelector('.fill-in-about').style.display ="none"
+
+
+}
+
+
+function validate(event) {
+    const permission = document.getElementById('feed1')
+    const about = document.getElementById('about')
+    
+
+    if (permission.checked && !about.value) {
+        event.preventDefault()
+        document.querySelector('.pop-ups').style.display ="flex"
+        document.querySelector('.fill-in-about').style.display ="flex"
+ 
+        window.location.href= "#header"
+
+    }
+}
+
+const comeBackAbout = document.getElementById('come-back-about')
+
+
+comeBackAbout.addEventListener('click', (event) => {
+    document.getElementById('click-about').click()
+})
