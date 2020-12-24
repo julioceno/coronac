abd47331b8d91cfca3ce6f6a0b5136f3fae2a3e5
Julio Nepomuceno <julioreplay@gmail.com>
2020-12-24 16:36:22 -0300

    inicializando congiguração do servidor

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express()
 app 
 
 .get('/', (req, resp) => {
-    return resp.send('<h1> Ola </h1>') 
+    return resp.send('<h1> Coronac </h1> <p> Ola Visitante, se voce está vendo isso nesse momento é porque o servidor ainda não configurado. Mas não se desanime, já estou ajustando esse erro </p>') 
 })
 
 .listen(process.env.PORT || 3000, console.log('ola'))
