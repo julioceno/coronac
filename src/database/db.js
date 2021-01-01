e9d03121d8c4b1a570ecfa58726cbc6e0b08171d
Julio Nepomuceno <julioreplay@gmail.com>
2021-01-01 09:48:47 -0300

    adicionando imagens github

--- src/database/db.js
+++ src/database/db.js
@@ -4,19 +4,19 @@ async function connect() {
     if (global.connection && global.connection.state !== 'disconnected') 
         return global.connection
 
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'coronac'
-    })   
-
     // const connection = await mysql.createConnection({
-    //     host: 'remotemysql.com',
-    //     user: 'ShqdGdGF8B',
-    //     password: '4CUdId3Gay',
-    //     database: 'ShqdGdGF8B'
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: '',
+    //     database: 'coronac'
     // })   
+
+    const connection = await mysql.createConnection({
+        host: 'remotemysql.com',
+        user: 'ShqdGdGF8B',
+        password: '4CUdId3Gay',
+        database: 'ShqdGdGF8B'
+    })   
     
     console.log('ready!')
     global.connection = connection
